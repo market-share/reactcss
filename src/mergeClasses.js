1cb95a0c5b20fa7be1384217a4154bf1667cab0b
Cooper Maruyama <cooperm@susteen.com>
2016-06-23 12:45:52 -0700

    Revert "eslint"
    This reverts commit 4c55690033bd634653a0fa313885bc2c6e7d93ed.

--- src/mergeClasses.js
+++ src/mergeClasses.js
@@ -1,13 +1,11 @@
 'use strict';
 
 export const mergeClasses = (classes, activeNames) => {
-  const styles = classes.default && Object.assign({}, classes.default) || {};
+  let styles = classes.default && Object.assign({}, classes.default) || {};
   activeNames.map((name) => {
     if (classes[name] !== null) {
       Object.assign(styles, classes[name]);
     }
-
-    return name;
   });
   return styles;
 };
