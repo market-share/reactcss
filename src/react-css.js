85d831d13e207c09a31680672dc2605f1befdd7c
Cooper Maruyama <cooperm@susteen.com>
2016-06-23 12:46:01 -0700

    Revert "export Hover and loop able"
    This reverts commit 7d1b39e9b1c1688d5864d941deadaa0ed5825d57.

--- src/react-css.js
+++ src/react-css.js
@@ -3,8 +3,6 @@
 import reactcss from './transform';
 import Component from './deprecated/Component';
 import inline from './inline';
-import Hover from './components/Hover';
-import loopable from './loopable';
 
 const ReactCSS = reactcss;
 
@@ -13,7 +11,5 @@ ReactCSS.inline = inline;
 ReactCSS.mixin = {
   css: inline,
 };
-ReactCSS.Hover = Hover;
-ReactCSS.loopable = loopable;
 
 export default ReactCSS;
