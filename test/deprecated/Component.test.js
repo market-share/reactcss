2999184c9645ef2ad3afd20b9460e5907cb2c929
Cooper Maruyama <cooperm@susteen.com>
2016-06-23 12:45:48 -0700

    Revert "Cleanup"
    This reverts commit a22d090a7c7764a1bc3377a1567dcab2c3a4dac1.

--- test/deprecated/Component.test.js
+++ test/deprecated/Component.test.js
@@ -1,12 +1,14 @@
-/* global describe, it, beforeEach, afterEach */
-
-import { expect } from '../helpers';
-const { Component } = require('../../src/reactcss');
-
-describe('Component', () => {
-  it('Check if its a function', () => {
-    expect(Component).to.be.function;
-  });
-
-  // Add more tests here to check for css and styles props
-});
+// 'use strict';
+//
+// import { expect } from '../helpers';
+// var { Component } = require('../../src/react-css');
+//
+// describe('Component', () => {
+//
+//   it('Check if its a function', () => {
+//     expect(Component).to.be.function;
+//   });
+//
+//   // Add more tests here to check for css and styles props
+//
+// });
